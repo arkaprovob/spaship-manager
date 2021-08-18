87b64bfa8cd99d4adafcc71dc7ae65684264dee8
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2021-08-18 22:42:25 +0530

    deployment : docker with sanity #2

--- packages/documentation/sidebars.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  someSidebar: {
-    SPAship: ["introduction", "contributing"],
-    "Getting Started": ["installation", "configuration", "user-guide/user-guide"],
-    Features: ["injection"],
-  },
-};
